c5c1c77ce92e2f36bc2f67a669d28868e1af8442
Özgür Kumaş <41588120+ozgurkumas@users.noreply.github.com>
2023-09-26 15:40:24 +0300

    detecting overflow of figures from the screen and creating new figure when main figure settled down

--- tetris.py
+++ tetris.py
@@ -17,9 +17,22 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+def minList(index, list_):
+    empty_list = []
+    for i in list_:
+        empty_list.append(i[index])
+    return min(empty_list)
+    
+def maxList(index, list_):
+    empty_list = []
+    for i in list_:
+        empty_list.append(i[index])
+    return max(empty_list)
+
 class Figure:
     def __init__(self):
         self.gridList = []
+        self.isActive = True
         self.color_list = [(0, 0, 255), (204, 96, 86), (208, 230, 101), (176, 26, 166), (40, 155, 184)]
         self.color = random.choice(self.color_list)
 
@@ -32,11 +45,14 @@ class Figure:
 class Figure1(Figure): # l block
     def __init__(self):
         self.gridList = [[120,0], [160,0], [200,0], [240, 0]]
+        self.isActive = True
         self.color_list = [(0, 0, 255), (204, 96, 86), (208, 230, 101), (176, 26, 166), (40, 155, 184)]
         self.color = random.choice(self.color_list)
       
 
 fig = Figure1()
+others_list = []
+
 
 while True:
 
@@ -45,25 +61,42 @@ while True:
             pygame.draw.rect(screen, black, (j * grid_scale, i * grid_scale, grid_scale, grid_scale))
             pygame.draw.rect(screen, white, (j * grid_scale, i * grid_scale, grid_scale, grid_scale),1)
 
+    if(fig.isActive==False):
+        others_list.append(fig)
+        fig = None
+        fig = Figure1()
+
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    for i in range(len(fig.gridList)):
-                        fig.gridList[i][0] += grid_scale
-                elif event.key == pygame.K_LEFT:
-                    for i in range(len(fig.gridList)):
-                        fig.gridList[i][0] -= grid_scale
-                elif event.key == pygame.K_DOWN:
-                    for i in range(len(fig.gridList)):
-                        fig.gridList[i][1] += grid_scale
+            if fig.isActive==True:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        if maxList(0, fig.gridList) < screen_width-grid_scale:
+                            for i in range(len(fig.gridList)):
+                                fig.gridList[i][0] += grid_scale
+                    elif event.key == pygame.K_LEFT:
+                        if minList(0, fig.gridList) > 0:
+                            for i in range(len(fig.gridList)):
+                                fig.gridList[i][0] -= grid_scale
+                    elif event.key == pygame.K_DOWN:
+                        if maxList(1, fig.gridList) < screen_height-grid_scale:
+                            for i in range(len(fig.gridList)):
+                                fig.gridList[i][1] += grid_scale
+                        else:
+                            fig.isActive = False
           
-    for i in range(len(fig.gridList)):
-        fig.gridList[i][1] += grid_scale
+    if fig.isActive == True:
+            if maxList(1, fig.gridList) < screen_height-grid_scale:
+                for i in range(len(fig.gridList)):
+                    fig.gridList[i][1] += grid_scale
+            else:
+                fig.isActive = False
 
     fig.update()
+    for object_ in others_list:
+        object_.update()
     
     pygame.display.update()
     clock.tick(fps)
